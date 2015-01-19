7e2a0d55d11c31dc85a40f16b20916d24e983c11
Charlie <icharlie.osu@gmail.com>
2015-01-18 16:32:51 -0800

    Add error check on position and fix email error issue

--- resources/views/users/form.blade.php
+++ resources/views/users/form.blade.php
@@ -5,19 +5,20 @@
     </div>
 </div>
 {!! $errors->first('name', '<div class="alert alert-danger alert-small col-sm-10 col-sm-offset-2"><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>:message</div>')  !!}
-<div class="form-group {!! $errors->has('name') ? 'has-error' : '' !!}">
+<div class="form-group {!! $errors->has('email') ? 'has-error' : '' !!}">
     {!! Form::label('email', 'Email', ['class' => 'col-sm-2 control-label']) !!}
     <div class="col-sm-10">
         {!! Form::text('email', null, ['class' => 'form-control']) !!}
     </div>
 </div>
 {!! $errors->first('email', '<div class="alert alert-danger alert-small col-sm-10 col-sm-offset-2"><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>:message</div>')  !!}
-<div class="form-group">
+<div class="form-group {!! $errors->has('position') ? 'has-error': ''!!}">
     {!! Form::label('position', 'Position', ['class' => 'col-sm-2 control-label']) !!}
     <div class="col-sm-10">
         {!! Form::text('position', null, ['class' => 'form-control']) !!}
     </div>
 </div>
+{!! $errors->first('position', '<div class="alert alert-danger alert-small col-sm-10 col-sm-offset-2"><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>:message</div>')  !!}
 <!-- {!! Form::label('password', 'Passowrd') !!}
     {!! Form::password('password', null) !!}
     {!! Form::label('password_confirmation', 'Passowrd Confirmation') !!}
