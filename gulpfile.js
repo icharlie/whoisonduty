3fcc79f73cdab77be8ac7d26aa0383c720c6f872
Charlie <icharlie.osu@gmail.com>
2015-02-28 00:01:15 -0800

    Fix node packages and gulpfile for test.

--- gulpfile.js
+++ gulpfile.js
@@ -13,8 +13,5 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.sass('app.scss')
-        .routes()
-        .events()
-        // .phpSpec();
-        .phpUnit();
+    .phpUnit();
 });
